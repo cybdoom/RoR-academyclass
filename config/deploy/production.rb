ef98389104e586cb96dc423b9234f95db7234438
Jonno Horsman <jonno@spleatapp.com>
2014-09-01 18:01:24 +0100

    Update server to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :rails_env, 'production'
 set :domain, "academyclass.com"
 set :application, "academyclass"
-role :web, 'public.srv-5cwwd.gb1.brightbox.com', user: 'ubuntu'
+role :web, 'public.srv-ktzv6.gb1.brightbox.com', user: 'ubuntu'
