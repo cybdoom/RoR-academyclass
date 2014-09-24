64c8f52ff002467e0e4af26d37b2edf1abad5095
Fiodor Veresciaka <fiodor.veresciaka@gmail.com>
2014-09-25 00:02:53 +0300

    Added model News

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140813085029) do
+ActiveRecord::Schema.define(:version => 20140924203143) do
 
   create_table "answers", :force => true do |t|
     t.integer "survey_response_id"
@@ -241,6 +241,16 @@ ActiveRecord::Schema.define(:version => 20140813085029) do
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
+  end
+
+  create_table "news", :force => true do |t|
+    t.string   "title"
+    t.string   "image"
+    t.text     "description"
+    t.text     "content"
+    t.boolean  "sticky",      :default => false, :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
   end
 
   create_table "news_pages", :force => true do |t|
