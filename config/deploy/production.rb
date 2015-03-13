1d96be81e017ad63c63107289bb0a43cc27ea523
Ubuntu <ubuntu@academyclass.com>
2015-03-13 17:51:39 +0000

    fix config for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :rails_env, 'production'
 set :domain, "academyclass.com"
 set :application, "academyclass"
-role :web, 'public.srv-ktzv6.gb1.brightbox.com', user: 'ubuntu'
+role :web, '217.174.249.40', user: 'ubuntu'
