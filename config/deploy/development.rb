e02b246f9e6e3a3b9191951892d43b6bd0eef91d
Ubuntu <ubuntu@academyclass.com>
2015-03-13 19:40:04 +0000

    fix development environment

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,5 +1,5 @@
 set :stage, :development
 set :rails_env, 'development'
-set :domain, "academyclass.com"
-set :application, "academyclass"
+set :domain, "test.academyclass.com"
+set :application, "test.academyclass.com"
 role :web, '217.174.249.40', user: 'ubuntu'
