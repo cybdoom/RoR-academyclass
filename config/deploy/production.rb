3bc0554a199ddf858e33796ec96e7098eda4f76f
Dima4ornenky <dima.chornenky@gmail.com>
2015-04-07 19:52:38 +0300

    fix deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :rails_env, 'production'
 set :domain, "academyclass.com"
 set :application, "academyclass"
 set :deploy_to, '/home/ubuntu/academyclass'
-role :web, '217.174.249.40', user: 'ubuntu'
+role :web, '109.107.35.66', user: 'ubuntu'
