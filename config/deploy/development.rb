5d012c9688bd48694d7ff65c24e585b7b9840aff
academyclass <academyclass@cp.academyclass.com>
2015-04-16 14:51:03 +0100

    try to deploy dev

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,6 +1,6 @@
-set :stage, :development
-set :rails_env, 'development'
+set :stage, :production
+set :rails_env, 'production'
 set :domain, "test.academyclass.com"
 set :application, "test.academyclass.com"
-set :deploy_to, '/home/ubuntu/test.academyclass.com'
-role :web, '217.174.249.40', user: 'ubuntu'
+set :deploy_to, '/home/academyclass/academyclass'
+role :web, '217.174.249.40', user: 'academyclass'
