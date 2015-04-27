951433444ec3f4030f9e9e1571d2f7c4dfd02a47
academyclass <academyclass@cp.academyclass.com>
2015-04-27 13:00:23 +0100

    test deploy restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,6 +42,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       execute :echo, release_path.join('tmp/restart.txt')
+      execute "echo 'Test deploy restart'"
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
